641e5253dba05697939c3a30bacc25f97b2fc545
Asukwo-comfort <asukwokulex24@gmail.com>
2022-08-18 15:47:19 -0700

    new file:   0-binary_to_uint.c 	new file:   1-print_binary.c 	new file:   2-get_bit.c 	new file:   3-set_bit.c 	new file:   4-clear_bit.c 	new file:   5-flip_bits.c 	new file:   README.md 	new file:   main.h

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: pointer to a string
+ *
+ * Return: converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base = 1, bin = 0, len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len])
+		len++;
+
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			bin += base;
+
+		base *= 2;
+		len--;
+	}
+	return (bin);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits you would need to flip to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits flipped.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff, base;
+	unsigned int count, i;
+
+	count = 0;
+	base = 1;
+	diff = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (base == (diff & base))
+			count++;
+		base <<= 1;
+	}
+	return (count);
+}
