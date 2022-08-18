388c3753c16303bc3e59121eaa1021d6206f4cd6
Asukwo-comfort <asukwokulex24@gmail.com>
2022-08-18 16:22:52 -0700

    new file:   100-get_endianness.c 	modified:   5-flip_bits.c 	modified:   main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,6 +12,7 @@ void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int *, unsigned int);
+int get_endianness(void);
 unsigned int flip_bits(unsigned long int, unsigned long int);
 
 #endif
